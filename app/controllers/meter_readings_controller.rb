d609911a9f29feb8b3878984257eefe9e71355fc
Björn Günzel <guenzel@blinker.net>
2011-06-22 23:47:51 +0200

    remove index method for meter_readings

--- app/controllers/meter_readings_controller.rb
+++ app/controllers/meter_readings_controller.rb
@@ -13,14 +13,14 @@ class MeterReadingsController < ApplicationController
 	end
     end
 
-    def index
-	readings = MeterReading.joins(:user).where(:users => {:ship_id => @current_user.ship_id}, :turn => @game.turn)
-
-	respond_to do |format|
-	    format.json {render :json => readings}
-	    format.xml {render :xml => readings}
-	end
-    end
+   #def index
+#	readings = MeterReading.joins(:user).where(:users => {:ship_id => @current_user.ship_id}, :turn => @game.turn)
+#
+#	respond_to do |format|
+#	    format.json {render :json => readings}
+#	    format.xml {render :xml => readings}
+#	end
+#   end
 
     def show
 	reading = @current_user.meter_readings.find(params[:id])
