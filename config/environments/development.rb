d920068fe5f5c185681ea399622cc006a21ccf2b
Björn Günzel <guenzel@blinker.net>
2011-06-29 01:19:20 +0200

    nothing changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ IslandsServer::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.app_domain = 'www.energy-streetfight.com'
 end
 
